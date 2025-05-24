3f8d642b7aab944344949c087d5168a5fe81d40f
Nacho <nachogalan@mac.com>
2025-05-24 21:58:24 +0200

    Corregido error de before_serving → before_first_request

--- config.py
+++ config.py
@@ -1,17 +1,18 @@
 import os
 
-MAIL_SERVER = os.getenv("MAIL_SERVER", "smtp.gmail.com")
-MAIL_PORT = int(os.getenv("MAIL_PORT", 587))
-MAIL_USE_TLS = os.getenv("MAIL_USE_TLS", "True") == "True"
-MAIL_USE_SSL = os.getenv("MAIL_USE_SSL", "False") == "True"
-MAIL_USERNAME = os.getenv("MAIL_USERNAME")
-MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
-MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER")
+MAIL_SERVER = 'smtp.strato.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USE_SSL = False
+MAIL_USERNAME = os.environ.get("EMAIL_ADDRESS")
+MAIL_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+MAIL_DEFAULT_SENDER = os.environ.get("EMAIL_ADDRESS")
+EMAIL_ADDRESS = os.environ.get("EMAIL_ADDRESS")
+EMAIL_PASSWORD = os.environ.get("EMAIL_PASSWORD")
 
-EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")
-EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
+# EMAIL_ADDRESS = "nachogalanmoreno@gmail.com"
+# EMAIL_PASSWORD = "wuer jfan kuzq csv lv"
 
-SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI", "sqlite:///registros.db")
-SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv("SQLALCHEMY_TRACK_MODIFICATIONS", "False") == "True"
-
-SECRET_KEY = os.getenv("SECRET_KEY", "clave-super-secreta")
+SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///instance/registros.db"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SECRET_KEY = os.environ.get("SECRET_KEY", "timeko_key")
